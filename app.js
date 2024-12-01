179be0b694059f1f929b58dca491e82754e6207d
HasanSiddiqui05 <155229366+HasanSiddiqui05@users.noreply.github.com>
2024-12-01 13:27:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+var fnum = "";
+var op = "";
+var lnum = "";
+var screen = document.querySelector(".display");
+
+function display(num) {
+    if(op === ""){
+        fnum += num;
+        screen.innerHTML = fnum;
+    } 
+    else{
+        lnum += num
+        screen.innerHTML = lnum
+    }
+}
+
+function back() {
+    if (op === "") {
+        // If no operator is selected, modify fnum
+        if (fnum !== "") {
+            fnum = fnum.slice(0, -1);
+            screen.innerHTML = fnum;
+        }
+    } else {
+        // If operator is selected, modify lnum
+        if (lnum !== "") {
+            lnum = lnum.slice(0, -1);
+            screen.innerHTML = lnum;
+        }
+    }
+}
+
+function clean() {
+    fnum = ""; 
+    op = "";   
+    lnum = "";     
+    screen.innerHTML = ""; 
+}
+
+function operations(ope){
+    if (fnum != ""){
+        op = ope;
+        screen.innerHTML = "";
+        update()
+    } 
+}
+
+function result(){
+    fnum = parseFloat(fnum)
+    lnum = parseFloat(lnum)
+    let res
+    switch(op){
+        case '+':
+            res = fnum + lnum
+            break;
+        case '-':
+            res = fnum - lnum
+            break;
+        case '*':
+            res = fnum * lnum
+            break;
+        case '/':
+            res = fnum / lnum
+            break;
+    }
+    screen.innerHTML = res; 
+    fnum = res.toString()
+    lnum = ""
+    op = ""
+}
